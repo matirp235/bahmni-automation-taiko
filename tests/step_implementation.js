43e58053ca3021eefdb343edea2f5d554bb7ea47
nityapriya.katta@thoughtworks.com <nityapriya@Nityas-MacBook-Pro.local>
2021-05-26 17:06:27 +0530

    added login

--- tests/step_implementation.js
+++ tests/step_implementation.js
@@ -6,8 +6,10 @@ const {
     write,
     closeBrowser,
     goto,
+    dropDown,
     press,
     screenshot,
+    below,
     above,
     click,
     checkBox,
@@ -43,48 +45,11 @@ gauge.customScreenshotWriter = async function () {
     return path.basename(screenshotFilePath);
 };
 
-step("Add task <item>", async (item) => {
-    await write(item, into(textBox("What needs to be done?")));
-    await press('Enter');
-});
 
-step("View <type> tasks", async function (type) {
-    await click(link(type));
-});
-
-step("Complete tasks <table>", async function (table) {
-    for (var row of table.rows) {
-        await click(checkBox(toLeftOf(row.cells[0])));
-    }
-});
-
-step("Clear all tasks", async function () {
-    await evaluate(() => localStorage.clear());
-});
-
-step("Open todo application", async function () {
-    await goto("todo.taiko.dev");
-});
-
-step("Must not have <table>", async function (table) {
-    for (var row of table.rows) {
-        assert.ok(!await text(row.cells[0]).exists(0, 0));
-    }
-});
-
-step("Must display <message>", async function (message) {
-    assert.ok(await text(message).exists(0, 0));
-});
-
-step("Add tasks <table>", async function (table) {
-    for (var row of table.rows) {
-        await write(row.cells[0]);
-        await press('Enter');
-    }
-});
-
-step("Must have <table>", async function (table) {
-    for (var row of table.rows) {
-        assert.ok(await text(row.cells[0]).exists());
-    }
+step("Login", async function() {
+    await goto("https://qa-amman.ehealthunit.org/bahmni/home/index.html#/login");
+    await write('superman', into(textBox("Username"),{force:true}));
+    await write('Admin123', into(textBox("Password"),{force:true}));
+    await dropDown('Location').select('OPD');
+    await click('Login', below('Location'));
 });
